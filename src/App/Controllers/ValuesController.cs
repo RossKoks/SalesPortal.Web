e0e24c2b7a02e8679fa0504005533871afccf0a6
dominikus1993 <dominikus1910@gmail.com>
2016-09-22 22:03:27 +0200

    add watch tool for web project

--- src/App/Controllers/ValuesController.cs
+++ src/App/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace SalesPortal.Web.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value2";
         }
 
         // POST api/values
