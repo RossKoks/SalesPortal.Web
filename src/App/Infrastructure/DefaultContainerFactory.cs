a0b8afaa9f9767e29622eb96c43b0fbe633cf60b
Dominik Kotecki <dominikus1993@users.noreply.github.com>
2016-09-25 11:39:52 +0200

    Autofac (#1)
    * install autofac
    
    * remove unused references
    
    * add autofac container to app
    
    * add container factory

---
+++ src/App/Infrastructure/DefaultContainerFactory.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalesPortal.Web.Infrastructure
+{
+    public class DefaultContainerFactory
+    {
+        public static IContainer Get(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            return builder.Build();
+        }
+    }
+}
